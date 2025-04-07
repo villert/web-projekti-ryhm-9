3e0c1300bfd621fba9e95bde91ec4b06ede04a3b
sisuteppo <t3tesi00@students.oamk.fi>
2025-04-07 09:39:30 +0300

    kysymykset ja vastaukset asetettu

---
+++ peli2/peli2.js
@@ -0,0 +1,4 @@
+let questions = ['Whats your name?', 'How are you?', "Who's dog is that?", 'your a great person!', 'Who am i?', 'To who it may concern', "I can't wait to learn more!"];
+let answers = ['incorrect', 'correct','correct','incorrect','incorrect','incorrect','correct'];
+let index = 0;
+let points = 0;
