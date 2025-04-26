2b9abc95f681ac93798129ba581eb7041af9e036
Vibbe00 <vibbe.pakkanen@gmail.com>
2025-04-27 02:56:23 +0300

    Html muokattu JS varten, Css lisätty muotoiluja painikkeille, JS toteutettu pelin toiminnot

--- peli3/js/peli3.js
+++ peli3/js/peli3.js
@@ -1,43 +1,90 @@
-var points = 0
-var questions = [
+var points = 0; //pisteet
+var index = 0; //Nykyinen kysymys
+var questions = [ //Kysymyslista
     {
-        question: "I went swimming in a _____ earlier today.\n(a) Lake\n(b) Puddle\n(c) Snowbank",
+        question: "I went swimming in a _____ earlier today.",
+        options: ["Lake", "Puddle", "Snowbank"],
         answer: "a"
     },
     {
-        question: "I _____ a bike to school.\n(a) Ride\n(b) Rode\n(c) Rided",
+        question: "I _____ a bike to school.",
+        options: ["Ride", "Rode", "Rided"],
         answer: "b"
     },
     {
-        question: "The cat is sleeping _____ the sofa.\n(a) Right\n(b) On\n(c) Over",
+        question: "The cat is sleeping _____ the sofa.",
+        options: ["Right", "On", "Over"],
         answer: "b"
     },
     {
-        question: "She _____ to the store once a week.\n(a) Going\n(b) Arrived\n(c) Goes",
+        question: "She _____ to the store once a week.",
+        options: ["Going", "Arrived", "Goes"],
         answer: "c"
     },
     {
-        question: "I have never _____ to paris before.\n(a) Thought\n(b) Traveled\n(c) Existed",
+        question: "I have never _____ to paris before.",
+        options: ["Thought", "Travelled", "Existed"],
         answer: "b"
     },
     {
-        question: "They _____ playing football when it started to rain.\n(a) Were\n(b) Are\n(c) Is",
+        question: "They _____ playing football when it started to rain.",
+        options: ["Were", "Are", "Is"],
         answer: "a"
     },
     {
-        question: "We need to buy some _____ for the birthday cake.\n(a) Wax\n(b) Wheat\n(c) Candles",
+        question: "We need to buy some _____ for the birthday cake.",
+        options: ["Wax", "Wheat", "Candles"],
         answer: "c"
     },
     {
-        question: "She _____ her homework before dinner.\n(a) Did\n(b) Do\n(c) Done",
+        question: "She _____ her homework before dinner.",
+        options: ["Did", "Do", "Done"],
         answer: "a"
     },
-]
+];
 
-
-for(var i=0; i < questions.length; i++){
-    var reaction = window.question(questions[i].question);
-    if(reaction == questions[i].answer){
-        points++;
+//Kysymysten ja vastaus painikkeiden näyttö
+function showQuestion() { 
+    var quizDiv = document.getElementById("quiz");
+    if (index < questions.length) {
+        var q = questions[index];
+        var html = `
+            <h5>${q.question}</h5>
+            <div>
+                <button class="quiz-button" onclick="checkanswer('a')">A) ${q.options[0]}</button>
+                <button class="quiz-button" onclick="checkanswer('b')">B) ${q.options[1]}</button>
+                <button class="quiz-button" onclick="checkanswer('c')">C) ${q.options[2]}</button>
+            </div>
+        `;
+        quizDiv.innerHTML = html;
+    } else {
+        document.getElementById("quiz").innerHTML = "";
+        document.getElementById("result").innerHTML = `<h3>You scored ${points} out of ${questions.length}!</h3>`;
+        localStorage.setItem("quizPoints", points);
     }
 }
+
+//Vastauksen tarkistus ja vastauksen jälkeinen viive ja painikkeiden sammuttaminen
+function checkanswer(choice) {
+    var resultDiv = document.getElementById("result");
+
+    if (choice == questions[index].answer){
+        points++;
+        resultDiv.innerHTML = "<p class='text-success'>Correct!</p>";
+    } else {
+        resultDiv.innerHTML = "<p class='text-danger'>Incorrect!</p>";
+    }
+
+    var buttons = document.querySelectorAll(".quiz-button");
+    buttons.forEach(button => button.disabled = true);
+
+    setTimeout(() => {
+        index++;
+        resultDiv.innerHTML = "";
+        showQuestion();
+    }, 3000)
+
+}
+
+//Kysymysten aloitus
+showQuestion();
