f487ecea32afd0c09fa7b4d290ed90870c17150d
topi <waakissa@gmail.com>
2025-04-27 05:47:02 +0300

    Lisäsin peli stuffit viimei githubii ja muokkasi noi piste jutut ja html linkkaa mu juttuu

---
+++ peli6/scripts/script.js
@@ -0,0 +1,78 @@
+const wordDisplay = document.querySelector(".word-display");
+const hangmanImage = document.querySelector(".hangman-box img");
+const guessesText = document.querySelector(".guesses-text b");
+const keyboardDiv = document.querySelector(".keyboard");
+const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = document.querySelector(".play-again");
+
+let currentWord, correctLetters, wrongGuesscount;
+const maxGuesses = 6;
+
+const resetGame = () => {
+    correctLetters = [];
+    wrongGuesscount = 0;
+    hangmanImage.src = `images/hangman-${wrongGuesscount}.svg`;
+    guessesText.innerText = `${wrongGuesscount} / ${maxGuesses}`;
+    keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+    wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li>`).join("");
+    gameModal.classList.remove("show");
+}
+
+/*randomi sanalista ja hintti siihe mukaa*/
+
+const getRandomWord = () => {
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+    currentWord = word;
+    console.log(word);
+    document.querySelector(".hint-text b").innerText = hint;
+    resetGame();
+
+}
+
+const gameOver = (isVictory) => {
+    setTimeout(() => {
+        const modalText = isVictory ? `You found the word:` : `The correct word was:`;
+        gameModal.querySelector("img").src = `images/${isVictory ? 'victory' : 'lost'}.gif`;
+        gameModal.querySelector("h4").innerText = `${isVictory ? 'Congrats!' : 'Game Over!'}`;
+        gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
+        gameModal.classList.add("show");
+    }, 300);
+}
+
+const initGame  = (button, clickedLetter) => {
+    if(currentWord.includes(clickedLetter)) {
+        [...currentWord].forEach((letter, index) => {
+            if(letter === clickedLetter) {
+                correctLetters.push(letter);
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        })
+        
+    } else {
+        wrongGuesscount++;
+        hangmanImage.src = `images/hangman-${wrongGuesscount}.svg`;
+    }
+
+    button.disabled = true;
+    guessesText.innerText = `${wrongGuesscount} / ${maxGuesses}` ;
+
+
+    if(wrongGuesscount === maxGuesses) return gameOver(false);
+    if(correctLetters.length === currentWord.length) return gameOver(true);
+}
+
+
+
+
+/*tehää keyboard kirjaimet*/
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+    button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)))
+    
+}
+/*saadaan random sana*/
+getRandomWord ();
+playAgainBtn.addEventListener("click" , getRandomWord);
