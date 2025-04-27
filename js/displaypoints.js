33cdb12e85e9cf7bb1b744736bd24d02210df224
villeraijas <vraijas24@students.oamk.fi>
2025-04-27 11:19:52 +0300

    pisteet

--- js/displaypoints.js
+++ js/displaypoints.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Haetaan tallennettu pistemäärä localStoragesta
     // Jos pistemäärää ei löydy, käytetään oletusarvoa 0
-    let score = localStorage.getItem("game1Score") || "0"; // Default to 0 if not found
-    let score = localStorage.getItem("game2Score") || "0"; // Default to 0 if not found
+    let game1 = localStorage.getItem("game1Score") || "0"; // Default to 0 if not found
+    let game2 = localStorage.getItem("game2Score") || "0"; // Default to 0 if not found
+    let game3 = localStorage.getItem("game3Score") || "0"; // Default to 0 if not found
 
     // Etsitään score elementti pisteet.html:stä ja laitetaan siihen pisteet
-    document.getElementById("game1-score").textContent = `Pisteet: ${score}`;
-    document.getElementById("game2-score").textContent = `Pisteet: ${score}`;
+    document.getElementById("game1-score").textContent = `Pisteet: ${game1}`;
+    document.getElementById("game2-score").textContent = `Pisteet: ${game2}`;
+    document.getElementById("game3-score").textContent = `Pisteet: ${game3}`;
 });
