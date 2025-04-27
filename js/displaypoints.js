2673da74df44f3bbf5c4dd5599e760c09c44b0eb
sisuteppo <t3tesi00@students.oamk.fi>
2025-04-27 11:58:00 +0300

    pisteitä taululle

--- js/displaypoints.js
+++ js/displaypoints.js
@@ -5,10 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
     let game2 = localStorage.getItem("game2Score") || "0"; 
     let game3 = localStorage.getItem("game3Score") || "0"; 
     let game4 = localStorage.getItem("game4Score") || "0"; 
+    let game5 = localStorage.getItem("game5Score") || "0"; 
 
     // Etsitään score elementti pisteet.html:stä ja laitetaan siihen pisteet
     document.getElementById("game1-score").textContent = `Pisteet: ${game1}`;
     document.getElementById("game2-score").textContent = `Pisteet: ${game2}`;
     document.getElementById("game3-score").textContent = `Pisteet: ${game3}`;
     document.getElementById("game4-score").textContent = `Pisteet: ${game4}`;
+    document.getElementById("game5-score").textContent = `Pisteet: ${game5}`;
 });
