bdaf17db58744690e33132120fee7005763f7faf
sisuteppo <t3tesi00@students.oamk.fi>
2025-04-27 11:48:04 +0300

    peli 4 navigointi

--- js/displaypoints.js
+++ js/displaypoints.js
@@ -4,9 +4,11 @@ document.addEventListener("DOMContentLoaded", function () {
     let game1 = localStorage.getItem("game1Score") || "0"; // Default to 0 if not found
     let game2 = localStorage.getItem("game2Score") || "0"; // Default to 0 if not found
     let game3 = localStorage.getItem("game3Score") || "0"; // Default to 0 if not found
+    let game4 = localStorage.getItem("game4Score") || "0"; // Default to 0 if not found
 
     // Etsitään score elementti pisteet.html:stä ja laitetaan siihen pisteet
     document.getElementById("game1-score").textContent = `Pisteet: ${game1}`;
     document.getElementById("game2-score").textContent = `Pisteet: ${game2}`;
     document.getElementById("game3-score").textContent = `Pisteet: ${game3}`;
+    document.getElementById("game4-score").textContent = `Pisteet: ${game4}`;
 });
